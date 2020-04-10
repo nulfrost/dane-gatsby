1391d8eda84db00e60cc2422b255e020452b54a5
Danex2 <khadane.miller@gmail.com>
2020-04-10 12:27:57 -0400

    fixed spacing for projects page on medium and large screens

--- src/components/components.js
+++ src/components/components.js
@@ -16,39 +16,39 @@ const fadeIn = keyframes`
 `
 
 export const Header = styled.header`
-  ${tw`flex flex-col sm:flex-row justify-between p-3`}
+  ${tw`flex flex-col justify-between p-3 sm:flex-row`}
 `
 
 export const NavLink = styled(Link)`
-  ${tw` text-gray-500 hover:text-purple-700 text-xl sm:text-lg`}
+  ${tw`text-xl text-gray-500 hover:text-purple-700 sm:text-lg`}
   transition: all 0.5s;
 `
 
 export const ExternalLink = styled.a`
-  ${tw`ml-5 text-gray-500 text-lg sm:text-sm hover:text-purple-700`}
+  ${tw`ml-5 text-lg text-gray-500 sm:text-sm hover:text-purple-700`}
   transition: all 0.5s
 `
 
 export const IntroText = styled.p`
-  ${tw`text-2xl sm:text-3xl text-center text-gray-900 font-thin`}
+  ${tw`text-2xl font-thin text-center text-gray-900 sm:text-3xl`}
   animation: ${fadeIn} 2s;
 `
 
 export const Project = styled.a`
-  ${tw`w-full sm:w-3/5 rounded-lg mb-3 flex flex-col hover:bg-gray-400 p-3`}
+  ${tw`flex flex-col w-full p-3 mb-3 rounded-lg md:w-3/4 lg:w-3/5 hover:bg-gray-400`}
 `
 export const ProjectTitle = styled.h2`
-  ${tw`text-lg uppercase font-bold underline mb-1`}
+  ${tw`mb-1 text-lg font-bold underline uppercase`}
 `
 
 export const Layout = styled.main`
-  ${tw`min-h-screen w-full sm:max-w-6xl mx-auto px-5 py-20 sm:py-20 sm:px-20 flex flex-col`}
+  ${tw`flex flex-col w-full min-h-screen px-5 py-20 mx-auto sm:max-w-6xl sm:py-20 sm:px-20`}
 `
 
 export const Content = styled.div`
-  ${tw`flex-auto p-3 flex flex-col items-center justify-center h-auto`}
+  ${tw`flex flex-col items-center justify-center flex-auto h-auto p-3`}
 `
 
 export const Footer = styled.footer`
-  ${tw`p-3 flex flex-col sm:flex-row items-start`}
+  ${tw`flex flex-col items-start p-3 sm:flex-row`}
 `
